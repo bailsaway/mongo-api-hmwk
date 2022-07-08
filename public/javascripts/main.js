fcea90c321fc03baeebfbadfeffeb9fa387da3d3
bailsaway <avbailey.6@gmail.com>
2022-07-08 17:58:26 +0100

    all calls except PUT

---
+++ public/javascripts/main.js
@@ -0,0 +1,119 @@
+const BASE_URL = "http://localhost:3333";
+
+const getList = document.getElementById("garage-list");
+
+//GET list of all vehicles
+try {
+  const response = await fetch(BASE_URL + "/api/v1/garage/");
+  if (!response.ok) throw response;
+  const data = await response.json();
+  data.forEach((element) => {
+    let li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(
+        `Vehicle ID: ${element._id}, Type: ${element.type}, Make: ${element.make}, Model: ${element.model}, Colour: ${element.colour}, BHP: ${element.bhp}, Year: ${element.year}`
+      )
+    );
+    getList.appendChild(li);
+  });
+} catch (err) {
+  console.log("err", err);
+}
+
+//function to make formData convered to json
+function createFormJSON(domForm) {
+  const newFormData = new FormData(domForm);
+  let domFormBody = JSON.stringify(Object.fromEntries(newFormData));
+  return domFormBody;
+}
+
+//post form to add vehicle to garage
+const postForm = document.getElementById("postForm");
+const postBtn = document.getElementById("postbtn");
+
+// POST method to /api/v1/garage/
+const asyncPostCall = async () => {
+  let body1 = createFormJSON(postForm);
+  try {
+    const response = await fetch("http://localhost:3333/api/v1/garage/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body1,
+    });
+    const data = await response.json();
+    console.log("Post call response", data);
+  } catch (error) {
+    console.log("Post error", error);
+  }
+};
+
+//post data event listener
+postBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  asyncPostCall();
+  location.reload(BASE_URL);
+});
+
+//DOM delete form
+const delForm = document.getElementById("delForm");
+const delBtn = document.getElementById("delBtn");
+
+//DELETE method to /api/v1/garage/
+const asyncDeleteCall = async () => {
+  let form = JSON.parse(createFormJSON(delForm));
+  let id = form._id;
+  try {
+    const response = await fetch(
+      `http://localhost:3333/api/v1/garage/?id=${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("Del call response", data);
+  } catch (error) {
+    console.log("Del error", error);
+  }
+};
+
+delBtn.addEventListener("click", (event) => {
+  event.preventDefault;
+  asyncDeleteCall();
+  location.reload(BASE_URL);
+});
+
+//DOM Put Form
+const putForm = document.getElementById("putForm");
+const putBtn = document.getElementById("putBtn");
+
+//PUT update to vehicle by ID
+const asyncPutUpdate = async () => {
+  try {
+    const response = await fetch(`http://localhost:3333/api/v1/garage/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: body1,
+    });
+    const data = await response.json();
+    console.log("Put call response", data);
+  } catch (error) {
+    console.log("Put error", error);
+  }
+};
+
+//function to delete _id from form body
+function deleteIdFromForm(domForm) {
+  const newForm = new FormData(domForm);
+  console.log("del func", newForm);
+}
+
+putBtn.addEventListener("click", () => {
+  deleteIdFromForm(putForm);
+});
